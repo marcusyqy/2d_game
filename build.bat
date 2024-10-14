dca5addf625614067a33d50b8b498273aa83bde6
Marcus Yong <marcusyqy@gmail.com>
2024-10-13 23:05:58 -0400

    Glfw

--- build.bat
+++ build.bat
@@ -9,15 +9,21 @@ if not exist .build mkdir .build
 if "%run%"=="1" set RUN=1
 
 set NAME=2d_game
-set CCFLAGS=
-REM -Werror -Wall -pedantic
+set CCFLAGS=-external:W0 -nologo -MP -FC -Zi  -wd4530 -utf-8 -WX -W3 -EHsc
+
+REM https://github.com/glfw/glfw/issues/1981#issuecomment-954047199
+set glfw_lib=..\external\GLFW\glfw3_mt.lib
 
 pushd .build
-if exist %NAME%.exe (
-  del %NAME%.exe
-)
+
 echo --[[COMPILING]]--
-clang ../src/main.c %CCFLAGS% -o %NAME%.exe -I../src
+cl ..\src\main.c %CCFLAGS% -Fe:%NAME%.exe -I..\src -external:I..\external -link %glfw_lib%
+
+if not %ERRORLEVEL% == 0 (
+  if exist %NAME%.exe (
+    del %NAME%.exe
+  )
+)
 
 if exist %NAME%.exe (
 if "%RUN%" == "1" (
--- src/main.c
+++ src/main.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 
+#define _GLFW_WIN32
+#include <GLFW/glfw3.h>
+#include <assert.h>
+
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "user32.lib")
+
+void error_callback(int error_code, const char* description) {
+  fprintf(stderr, "glfw failed with %d : %s", error_code, description);
+}
 
 int main(int argument_count, const char *arguments[]) {
   (void)argument_count;
   (void)arguments;
 
-  fprintf(stdout, "Hello world!\n");
+
+  assert(glfwInit());
+  glfwSetErrorCallback(error_callback);
+
+
+  GLFWwindow *window = glfwCreateWindow(680, 480, "Hello world", NULL, NULL);
+  if(!window) {
+    goto end;
+  }
+
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+end:
+  glfwTerminate();
   return 0;
 }
